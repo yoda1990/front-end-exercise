c5f11478bb879401b090c6d0afc7a09f8d668433
You-Hmily <706097141@qq.com>
2019-09-08 17:53:27 +0800

    重新初始化项目，开始用于实战项目联系，规范上传代码

--- react/my-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import MyApp from './components/MyApp';
-import './index.css'
-
-ReactDom.render(<MyApp/>,document.getElementById('root'));
